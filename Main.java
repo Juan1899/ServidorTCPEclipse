3d382b3cbc40dd6706565bef12b584bc26971f8f
Juan <juanmanueldelarosaenriquez@gmail.com>
2020-03-03 13:15:25 -0500

    Primer commit equis de
    Soy el dios de la programación

---
+++ Main.java
@@ -0,0 +1,88 @@
+package main;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("main.Main");
+
+	}
+
+	
+	
+	public void settings() {
+		size(500,500); 
+	}
+	
+	public void setup() {
+		
+		// Este elemento nos permite esperar una conexión. 
+		
+		new Thread(
+				
+				() -> {
+					
+						try {
+						ServerSocket server = new ServerSocket(5000);
+						
+						// Esta linea me permite aceptar la conexión entrante. 
+						System.out.println("Esperando...");
+						
+						Socket socket = server.accept();
+						System.out.println("Conexión aceptada");
+						
+						InputStream is =  socket.getInputStream();
+						
+			           
+			            InputStreamReader isr = new InputStreamReader(is);
+
+			          
+			            BufferedReader reader = new BufferedReader(isr);
+			            
+			            while(true) {
+			            
+			            String line = reader.readLine(); 
+			            System.out.println(line);
+			            
+			            switch(line) {
+			            
+			            
+			            case "UP":
+			            	
+			            	y-=5;
+			            	break; 
+			            }
+			            
+			           
+			            }
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} 
+					
+				
+				}
+				
+				
+				).start(); 
+		
+		
+	}
+	
+	int x = 250, y = 250;
+	public void draw() {
+		
+		background(0);
+		
+		fill(255,100,100);
+		ellipse(x,y,50,50); 
+	}
+}
